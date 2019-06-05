dfab799cfd8add46ee67143ad66e5e184628778f
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-06-05 11:20:12 +0100

    Rover turns right to face East

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,5 +1,7 @@
 public class MarsRover {
-    public String execute() {
+    public String execute(String instructions) {
+        if(instructions.equals("R"))
+            return "0,0,E";
         return "0,0,N";
     }
 }
--- src/test/java/MarsRoverShould.java
+++ src/test/java/MarsRoverShould.java
@@ -1,13 +1,17 @@
 import org.junit.jupiter.api.Test;
 
-import javax.sql.rowset.CachedRowSet;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MarsRoverShould {
     @Test
     void start_at_0_0_N() {
         MarsRover rover = new MarsRover();
-        assertEquals("0,0,N", rover.execute());
+        assertEquals("0,0,N", rover.execute(""));
+    }
+
+    @Test
+    void rotate_right() {
+        MarsRover rover = new MarsRover();
+        assertEquals("0,0,E", rover.execute("R"));
     }
 }
