7de3391ed70380bc07cf424a5ee13b06bc6368ed
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-06-05 11:24:50 +0100

    Move rover initialisation to setUp

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -2,6 +2,8 @@ public class MarsRover {
     public String execute(String instructions) {
         if(instructions.equals("R"))
             return "0,0,E";
+        if(instructions.equals("RR"))
+            return "0,0,S";
         return "0,0,N";
     }
 }
--- src/test/java/MarsRoverShould.java
+++ src/test/java/MarsRoverShould.java
@@ -1,17 +1,31 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MarsRoverShould {
+
+    private MarsRover rover;
+
+    @BeforeEach
+    void setUp() {
+        rover = new MarsRover();
+    }
+
     @Test
     void start_at_0_0_N() {
-        MarsRover rover = new MarsRover();
         assertEquals("0,0,N", rover.execute(""));
     }
 
     @Test
     void rotate_right() {
-        MarsRover rover = new MarsRover();
         assertEquals("0,0,E", rover.execute("R"));
+    }
+
+    @Test
+    void rotates_right_twice() {
+        assertEquals("0,0,S", rover.execute("RR"));
     }
 }
