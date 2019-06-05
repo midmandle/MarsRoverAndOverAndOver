fe05ccb56de118d08679251d052e8bd0ac6a49c8
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-06-05 14:15:20 +0100

    Consolidate right turn tests into parameterized test

--- src/test/java/MarsRoverShould.java
+++ src/test/java/MarsRoverShould.java
@@ -1,7 +1,12 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import javax.swing.*;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -14,28 +19,22 @@ public class MarsRoverShould {
         rover = new MarsRover();
     }
 
+    private static Stream<Arguments> rotateRightTests() {
+        return Stream.of(
+                Arguments.arguments("R", "0,0,E"),
+                Arguments.arguments("RR", "0,0,S"),
+                Arguments.arguments("RRR", "0,0,W"),
+                Arguments.arguments("RRRR", "0,0,N")
+        );
+    }
+    @ParameterizedTest
+    @MethodSource("rotateRightTests")
+    void rotates_right(String instructions, String expectedOutput) {
+        assertEquals(expectedOutput, rover.execute(instructions));
+    }
+
     @Test
     void start_at_0_0_N() {
         assertEquals("0,0,N", rover.execute(""));
-    }
-
-    @Test
-    void rotate_right() {
-        assertEquals("0,0,E", rover.execute("R"));
-    }
-
-    @Test
-    void rotates_right_twice() {
-        assertEquals("0,0,S", rover.execute("RR"));
-    }
-
-    @Test
-    void rotate_right_three_times() {
-        assertEquals("0,0,W", rover.execute("RRR"));
-    }
-
-    @Test
-    void rotate_full_circle_right() {
-        assertEquals("0,0,N", rover.execute("RRRR"));
     }
 }
