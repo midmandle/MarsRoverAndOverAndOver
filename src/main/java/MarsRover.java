c16a71e6daa69eba853aab0c5353c798185ccb89
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-06-06 11:01:03 +0100

    Extract generateLocationString()

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,11 +1,32 @@
 public class MarsRover {
+
+    private int xCoordinate;
+    private int yCoordinate;
+    private char direction;
+
+    public MarsRover() {
+        xCoordinate = 0;
+        yCoordinate = 0;
+        direction = 'N';
+    }
+
     public String execute(String instructions) {
-        if(instructions.equals("RRR"))
-            return "0,0,W";
-        if(instructions.equals("RR"))
-            return "0,0,S";
-        if(instructions.equals("R"))
-            return "0,0,E";
-        return "0,0,N";
+        if(instructions.equals("RRR")){
+            this.direction = 'W';
+            return generateLocationString();
+        }
+        if(instructions.equals("RR")){
+            this.direction = 'S';
+            return generateLocationString();
+        }
+        if(instructions.equals("R")) {
+            this.direction = 'E';
+            return generateLocationString();
+        }
+        return this.generateLocationString();
+    }
+
+    private String generateLocationString() {
+        return this.xCoordinate + "," + this.yCoordinate + "," + this.direction;
     }
 }
