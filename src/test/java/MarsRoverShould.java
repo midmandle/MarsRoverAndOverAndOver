032e6950362bc9bca8514815abeaa6479bb92f1b
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-06-06 10:51:43 +0100

    Refactor rover initialisation into setUp()

--- src/test/java/MarsRoverShould.java
+++ src/test/java/MarsRoverShould.java
@@ -1,23 +1,29 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MarsRoverShould {
+
+    private MarsRover rover;
+
+    @BeforeEach
+    void setUp() {
+        rover = new MarsRover();
+    }
+
     @Test
     void start_at_0_0_N() {
-        MarsRover rover = new MarsRover();
         assertEquals("0,0,N", rover.execute(""));
     }
 
     @Test
     void rotate_right_to_face_east() {
-        MarsRover rover = new MarsRover();
         assertEquals("0,0,E", rover.execute("R"));
     }
 
     @Test
     void rotate_right_twice_to_face_south() {
-        MarsRover rover = new MarsRover();
         assertEquals("0,0,S", rover.execute("RR"));
     }
 }
