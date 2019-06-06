ce6eeeabca2448fa7a39d58f331e09e185ffe62f
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-06-06 11:14:12 +0100

    Extract execution loop

---
+++ src/main/java/Coordinates.java
@@ -0,0 +1,14 @@
+public class Coordinates {
+    int xCoordinate;
+    int yCoordinate;
+
+    public Coordinates(int xCoordinate, int yCoordinate) {
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+    }
+
+    @Override
+    public String toString() {
+        return xCoordinate + "," + yCoordinate;
+    }
+}
--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,32 +1,56 @@
 public class MarsRover {
 
-    private int xCoordinate;
-    private int yCoordinate;
+    private final Coordinates coordinates;
     private char direction;
 
     public MarsRover() {
-        xCoordinate = 0;
-        yCoordinate = 0;
         direction = 'N';
+        coordinates = new Coordinates(0, 0);
     }
 
     public String execute(String instructions) {
-        if(instructions.equals("RRR")){
-            this.direction = 'W';
-            return generateLocationString();
-        }
-        if(instructions.equals("RR")){
-            this.direction = 'S';
-            return generateLocationString();
-        }
-        if(instructions.equals("R")) {
-            this.direction = 'E';
-            return generateLocationString();
+        for (char command :
+                instructions.toCharArray()) {
+            if (command == 'R')
+                rotateRight();
         }
         return this.generateLocationString();
     }
 
+    private void rotateRight() {
+        switch (direction) {
+            case 'N':
+                faceEast();
+                break;
+            case 'E':
+                faceSouth();
+                break;
+            case 'S':
+                faceWest();
+                break;
+            case 'W':
+                faceNorth();
+                break;
+        }
+    }
+
+    private void faceNorth() {
+        this.direction = 'N';
+    }
+
+    private void faceEast() {
+        this.direction = 'E';
+    }
+
+    private void faceSouth() {
+        this.direction = 'S';
+    }
+
+    private void faceWest() {
+        this.direction = 'W';
+    }
+
     private String generateLocationString() {
-        return this.xCoordinate + "," + this.yCoordinate + "," + this.direction;
+        return this.coordinates + "," + this.direction;
     }
 }
