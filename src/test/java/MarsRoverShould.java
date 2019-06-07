bbbd481e3068a1b8f37ed4c94f64d4c6a94e08d1
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-06-07 11:24:17 +0100

    Move rover instantiation to setUp method

--- src/test/java/MarsRoverShould.java
+++ src/test/java/MarsRoverShould.java
@@ -1,29 +1,34 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MarsRoverShould {
+
+    private MarsRover rover;
+
+    @BeforeEach
+    void setUp() {
+        rover = new MarsRover();
+    }
+
     @Test
     void start_at_0_0_N() {
-        MarsRover rover = new MarsRover();
         assertEquals("0,0,N", rover.execute(""));
     }
 
     @Test
     void rotate_right_to_face_east() {
-        MarsRover rover = new MarsRover();
         assertEquals("0,0,E", rover.execute("R"));
     }
 
     @Test
     void rover_rotates_right_twice_to_face_south() {
-        MarsRover rover = new MarsRover();
         assertEquals("0,0,S", rover.execute("RR"));
     }
 
     @Test
     void rover_rotates_right_three_times_to_face_west() {
-        MarsRover rover = new MarsRover();
         assertEquals("0,0,W", rover.execute("RRR"));
     }
 }
