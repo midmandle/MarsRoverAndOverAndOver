36325ab99c0b1482857410a4d89a15061e3c3715
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-06-07 11:42:51 +0100

    Extract constant

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,12 +1,13 @@
 public class MarsRover {
     public static final char TURN_RIGHT = 'R';
+    public static final char TURN_LEFT = 'L';
     private final Location location = new Location();
 
     public String execute(String instructions) {
         for (char instruction : instructions.toCharArray()) {
             if (instruction == TURN_RIGHT)
                 location.rotateRight();
-            if(instruction == 'L')
+            if(instruction == TURN_LEFT)
                 location.rotateLeft();
         }
         return location.generateLocationString();
