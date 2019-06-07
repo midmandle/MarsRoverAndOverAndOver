0f6cbdfe9cc7a098808ab35ddba8db98ac1111de
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-06-07 11:21:05 +0100

    Extract constant WEST

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -3,12 +3,13 @@ public class MarsRover {
     public static final char NORTH = 'N';
     public static final char SOUTH = 'S';
     public static final char EAST = 'E';
+    public static final char WEST = 'W';
     private final Coordinates coordinates = new Coordinates();
     private Character direction = NORTH;
 
     public String execute(String instructions) {
         if(instructions.equals("RRR"))
-            direction = 'W';
+            direction = m WEST;
         if(instructions.equals("RR"))
             direction = SOUTH;
         if(instructions.equals("R"))
