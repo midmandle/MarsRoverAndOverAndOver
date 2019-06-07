23929c8d164a16fa0d81e7fa19c82b2c81b9a516
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-06-07 11:08:58 +0100

    Rover starts at 0,0,N

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,2 +1,5 @@
 public class MarsRover {
+    public String execute(String instructions) {
+        return "0,0,N";
+    }
 }
--- src/test/java/MarsRoverShould.java
+++ src/test/java/MarsRoverShould.java
@@ -1,3 +1,13 @@
-public class MarsRoverShould {
+import org.junit.jupiter.api.Test;
 
+import javax.swing.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MarsRoverShould {
+    @Test
+    void start_at_0_0_N() {
+        MarsRover rover = new MarsRover();
+        assertEquals("0,0,N", rover.execute(""));
+    }
 }
