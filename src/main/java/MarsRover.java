8abd15d547138b3b06a3fea5d9ff684bfb5ebc1a
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-06-07 11:18:29 +0100

    Extract direction constants

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,13 +1,16 @@
 public class MarsRover {
 
+    public static final char NORTH = 'N';
+    public static final char SOUTH = 'S';
+    public static final char EAST = 'E';
     private final Coordinates coordinates = new Coordinates();
-    private String direction = "N";
+    private Character direction = NORTH;
 
     public String execute(String instructions) {
         if(instructions.equals("RR"))
-            direction = "S";
+            direction = SOUTH;
         if(instructions.equals("R"))
-            direction = "E";
+            direction = EAST;
 
         return coordinates.xCoordinate + "," + coordinates.yCoordinate + "," + direction;
     }
