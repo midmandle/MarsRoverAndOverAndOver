05acbd87acfe6471e2d358799de03f1cd5bb7507
Alasdair Smith <alasdairsmith0491@gmail.com>
2019-06-07 11:33:37 +0100

    Extract rotateRight method

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -6,22 +6,27 @@ public class MarsRover {
     public String execute(String instructions) {
         for (char instruction :
                 instructions.toCharArray()) {
-            if (instruction == 'R')
-                switch (direction) {
-                    case NORTH:
-                        direction = Compass.EAST;
-                        break;
-                    case SOUTH:
-                        direction = Compass.WEST;
-                        break;
-                    case EAST:
-                        direction = Compass.SOUTH;
-                        break;
-                    case WEST:
-                        direction = Compass.NORTH;
-                        break;
-                }
+            if (instruction == 'R') {
+                rotateRight();
+            }
         }
         return coordinates.xCoordinate + "," + coordinates.yCoordinate + "," + direction;
+    }
+
+    private void rotateRight() {
+        switch (direction) {
+            case NORTH:
+                direction = Compass.EAST;
+                break;
+            case SOUTH:
+                direction = Compass.WEST;
+                break;
+            case EAST:
+                direction = Compass.SOUTH;
+                break;
+            case WEST:
+                direction = Compass.NORTH;
+                break;
+        }
     }
 }
